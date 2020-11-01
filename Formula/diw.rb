ce3d97f85a533750521e5a1769ef2a712976559f
marcofaul <marcofaul@protonmail.com>
2020-11-01 10:34:50 +0100

    change to placeholder

--- Formula/diw.rb
+++ Formula/diw.rb
@@ -1,9 +1,9 @@
 class Diw < Formula
   desc "Diw is a CLI utility"
   homepage "https://dasistweb.de"
-  version "v0.0.1"
-  url "https://github.com/marcofaul/diw/archive/v0.0.1.tar.gz"
-  sha256 "42d8dbaf0ec7ea5208ff92fe7ad5bf0d9449e54e2608da145da9e1b116e8afd1"
+  url "{{{tarballUrl}}}"
+  version "{{version}}"
+  sha256 "{{sha256}}"
   head "https://ithub.com:marcofaul/diw.git", :branch => "develop"
 
   def install
