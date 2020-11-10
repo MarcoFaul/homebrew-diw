feaf3920c19b8c1e7eb351fe576d54c62a57d032
marcofaul <marcofaul@protonmail.com>
2020-11-10 20:48:57 +0100

    update description

--- Formula/diw.rb
+++ Formula/diw.rb
@@ -4,7 +4,6 @@ class Diw < Formula
   url "https://github.com/MarcoFaul/diw/archive/v0.0.3.3.tar.gz"
   version "0.0.3.3"
   sha256 "1d25656ebc7746500ead45ac3669de5664ba713de753bf4a8e7141fbf4a1d849"
-  head "https://ithub.com:marcofaul/diw.git", :branch => "develop"
 
   def install
     prefix.install Dir["*"]
@@ -12,7 +11,20 @@ class Diw < Formula
 
   def caveats
     <<~EOS
-      diw ftw
+      diw is a CLI utility made for DIW
+
+                         \    /
+                          \  /
+      |\\  //|             ||
+       \ \/  /             ||
+        |   |              ||
+        |   |              ||
+         \   \------------------------\
+          \                       o    \
+           \-                           \
+            \                 \__________\
+             \-                          /        Marco Faul at dasistweb
+               \________________________/
     EOS
   end
 end
