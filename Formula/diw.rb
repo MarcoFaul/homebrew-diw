e7e58e58d29ccef50fc3cf26aa9298f660b55785
marcofaul <marcofaul@protonmail.com>
2020-11-11 18:00:04 +0100

    v1.0.0

--- Formula/diw.rb
+++ Formula/diw.rb
@@ -5,26 +5,29 @@ class Diw < Formula
   version "0.0.3.3"
   sha256 "1d25656ebc7746500ead45ac3669de5664ba713de753bf4a8e7141fbf4a1d849"
 
+  depends_on "composer"
+
   def install
+    composer install
     prefix.install Dir["*"]
+    bin.install 'diw'
+    doc.install 'README'
   end
 
   def caveats
     <<~EOS
-      diw is a CLI utility made for DIW
+      Diw is a CLI utility made for DIW
 
-                         \    /
-                          \  /
-      |\\  //|             ||
-       \ \/  /             ||
-        |   |              ||
-        |   |              ||
-         \   \------------------------\
-          \                       o    \
-           \-                           \
-            \                 \__________\
-             \-                          /        Marco Faul at dasistweb
-               \________________________/
+      To start diw simply run:
+
+            diw
+
+        Documentation is available at: https://diw-tool.netlify.app/
+        Created by Marco Faul
     EOS
   end
+
+    test do
+      system "#{bin}/diw"
+    end
 end
