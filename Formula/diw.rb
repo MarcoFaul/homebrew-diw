65f2076d7740093dc96dfa1f336b2df2fb2d6486
MarcoFa <marcofaul@protonmail.com>
2020-11-14 20:17:32 +0100

    diw 0.1.8
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/diw.rb
+++ Formula/diw.rb
@@ -1,9 +1,9 @@
 class Diw < Formula
   desc "Diw is a CLI utility"
   homepage "https://dasistweb.de"
-  url "https://github.com/MarcoFaul/diw/archive/v0.1.7.tar.gz"
-  version "0.1.7"
-  sha256 "31a23f2ddc2e7dba8cb92af96f50e54a368ce8fc3f63525d55affc4b84434e4b"
+  url "https://github.com/MarcoFaul/diw/archive/v0.1.8.tar.gz"
+  version "0.1.8"
+  sha256 "28ff388771877844739108e14feb9691d19fddb1e7843013e883156602e008d9"
 
   def install
     prefix.install Dir["*"]
