319d165093d5e9d3d08ad4fd627cfc37872d8997
marcofaul <marcofaul@protonmail.com>
2021-10-16 13:19:11 +0200

    add diw-override config.yaml

--- Formula/diw.rb
+++ Formula/diw.rb
@@ -4,6 +4,20 @@ class Diw < Formula
   url "https://github.com/MarcoFaul/diw/releases/download/v0.9.3.2/diw-0.9.3.2.tar.gz"
   version "0.9.3.2"
   sha256 "a78d7e14839b0e5f33fa017a384fd2dbd4f2da64c51de0df6dc512e1f3dc64a0"
+  license ""
+
+  def post_install
+    if !(File.exist?((etc/"diw-override.config.yaml"))) then
+      (etc/"diw-override.config.yaml").write override_conf
+    end
+  end
+
+  def override_conf; <<~EOS
+    docker:
+      container:
+  EOS
+  end
+
 
   def install
     prefix.install Dir["*"]
